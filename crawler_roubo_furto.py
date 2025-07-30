8291ef8af35d55863a920847113dc039d5b14bdc
henriqueyjw <52829906+henriqueyjw@users.noreply.github.com>
2025-07-30 18:42:58 -0300

    Update crawler_roubo_furto.py

--- crawler_roubo_furto.py
+++ crawler_roubo_furto.py
@@ -1,94 +1,89 @@
-import requests
-from datetime import date
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from urllib.parse import quote
-from tqdm import tqdm
-import warnings
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from selenium.webdriver.support.ui import Select
+import time
+import re
 import pandas as pd
 
-# Remove warnings a cada requisição
-warnings.simplefilter('ignore', InsecureRequestWarning)
+path = 'C:/Users/henri/Desktop/programs'
+final_path = 'E:/artigo3/dados_crime'
 
-# Função principal para extração
-def extract_decisoes_tjsp_grau1(links):
-    dados = []  # Lista para armazenar os dados
-    
-    for link in links:
-        session = requests.Session()
-        req = session.get(link, verify=False)
-        
-        if "Não foi encontrado nenhum resultado correspondente à busca realizada" in req.text:
-            print(f"Nenhum resultado encontrado para a URL: {link}")
-            continue
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=800,600")
+driver = webdriver.Chrome(executable_path = os.path.join(path,'chromedriver.exe'), options = chrome_options)
 
-        soupPrincipal = BeautifulSoup(req.content, 'html.parser')
+links = [
+    'https://esaj.tjsp.jus.br/cjpg/pesquisar.do?conversationId=&dadosConsulta.pesquisaLivre=roubo+OU+furto&tipoNumero=UNIFICADO&numeroDigitoAnoUnificado=&foroNumeroUnificado=&dadosConsulta.nuProcesso=&dadosConsulta.nuProcessoAntigo=&classeTreeSelection.values=&classeTreeSelection.text=&assuntoTreeSelection.values=&assuntoTreeSelection.text=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&cdAgente=&nmAgente=&dadosConsulta.dtInicio=01%2F01%2F2021&dadosConsulta.dtFim=31%2F12%2F2021&varasTreeSelection.values=&varasTreeSelection.text=&dadosConsulta.ordenacao=DESC'
+    ]
 
-        # Recupera o número total de resultados
-        totalResultados = int(soupPrincipal.find("div", {"id": "resultados"}).find_all("table")[0].tr.td.text.strip().split("de ")[1])
-        totalPaginas = totalResultados // 10 if totalResultados % 10 == 0 else (totalResultados // 10) + 1
-        
-        print(f"Iniciando coleta de dados para a URL: {link}")
-        for i in tqdm(range(1, totalPaginas + 1)):
-            url_paginate = f"https://esaj.tjsp.jus.br/cjpg/pesquisar.do?conversationId=&dadosConsulta.pesquisaLivre=roubo+OU+furto&tipoNumero=UNIFICADO&numeroDigitoAnoUnificado=&foroNumeroUnificado=&dadosConsulta.nuProcesso=&dadosConsulta.nuProcessoAntigo=&classeTreeSelection.values=&classeTreeSelection.text=&assuntoTreeSelection.values=&assuntoTreeSelection.text=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&cdAgente=&nmAgente=&dadosConsulta.dtInicio=01%2F01%2F2021&dadosConsulta.dtFim=31%2F12%2F2021&varasTreeSelection.values=&varasTreeSelection.text=&dadosConsulta.ordenacao=DESC"
-            req = session.get(url_paginate, verify=False)
-            soupPrincipal = BeautifulSoup(req.content, 'html.parser')
-            
-            for target in soupPrincipal.find_all('tr', {'class': 'fundocinza1'}):
-                contador = 0
-                linha = {
-                    "numero_cnj": None,
-                    "classe": None,
-                    "assunto": None,
-                    "magistrado": None,
-                    "comarca": None,
-                    "foro": None,
-                    "vara": None,
-                    "data_disponibilizacao": None,
-                    "decisao": None,
-                }
-
-                for info in target.find_all('tr', {'class': 'fonte'}):
+rows = list()
+file_number = 27
+for url in links:
+    driver.get(url)
+    for i in range(8000):
+        html_source = driver.page_source
+        soup = BeautifulSoup(html_source,"html.parser")
+        for target in soup.find_all('tr',{'class':'fundocinza1'}):
+            contador = 0
+            flag = False
+            linha = dict()
+            for info in target.find_all('td',{'align':'left'})[1:]:
+                if len(target.find_all('td',{'align':'left'})[1:]) == 8:
                     if contador == 0:
-                        linha["numero_cnj"] = info.span.text.strip()
+                        linha['numero_cnj'] = re.sub(r'\s{2,100}','',info.text)
                     elif contador == 1:
-                        linha["classe"] = info.td.text.strip().split("\n\t")[-1].strip()
+                        linha['classe'] = None
+                        linha['assunto'] = re.sub(r'\s{2,100}','',info.text)[8:]
                     elif contador == 2:
-                        linha["assunto"] = info.td.text.strip().split("\n\t")[-1].strip()
+                        linha['magistrado'] = re.sub(r'\s{2,100}','',info.text)[11:]
                     elif contador == 3:
-                        linha["magistrado"] = info.td.text.strip().split("\n\t")[-1].strip()
+                        linha['comarca'] = re.sub(r'\s{2,100}','',info.text)[8:]
                     elif contador == 4:
-                        linha["comarca"] = info.td.text.strip().split("\n\t")[-1].strip()
+                        linha['foro'] = re.sub(r'\s{2,100}','',info.text)[5:]
                     elif contador == 5:
-                        linha["foro"] = info.td.text.strip().split("\n\t")[-1].strip()
+                        linha['vara'] = re.sub(r'\s{2,100}','',info.text)[5:]
                     elif contador == 6:
-                        linha["vara"] = info.td.text.strip().split("\n\t")[-1].strip()
+                        linha['data'] = re.sub(r'\s{2,100}','',info.text)[25:]
                     elif contador == 7:
-                        linha["data_disponibilizacao"] = info.td.text.strip().split("\n\t")[-1].strip()
-                    elif contador == 8:
-                        linha["decisao"] = info.td.find_all('div', {'style': 'display: none;'})[-1].span.text.strip()
-
+                        linha['decisao'] = re.sub(r'[\n\t]{1,100}','',info.text)
                     contador += 1
+                if len(target.find_all('td',{'align':'left'})[1:]) == 9:
+                    if contador == 0:
+                        linha['numero_cnj'] = re.sub(r'\s{2,100}','',info.text)
+                    elif contador == 1:
+                        linha['classe'] = re.sub(r'\s{2,100}','',info.text)[7:]
+                    elif contador == 2:
+                        linha['assunto'] = re.sub(r'\s{2,100}','',info.text)[8:]
+                    elif contador == 3:
+                        linha['magistrado'] = re.sub(r'\s{2,100}','',info.text)[11:]
+                    elif contador == 4:
+                        linha['comarca'] = re.sub(r'\s{2,100}','',info.text)[8:]
+                    elif contador == 5:
+                        linha['foro'] = re.sub(r'\s{2,100}','',info.text)[5:]
+                    elif contador == 6:
+                        linha['vara'] = re.sub(r'\s{2,100}','',info.text)[5:]
+                    elif contador == 7:
+                        linha['data'] = re.sub(r'\s{2,100}','',info.text)[25:]
+                    elif contador == 8:
+                        linha['decisao'] = re.sub(r'[\n\t]{1,100}',' ',info.text)
+                    contador += 1
+            rows.append(linha)
+        try:
+            if i == 0:
+                driver.find_elements_by_xpath('//*[@id="resultados"]/table[3]/tbody/tr[1]/td[2]/div/a[5]')[0].click()
+                time.sleep(4)
+            else:
+                driver.find_elements_by_xpath('//*[@id="resultados"]/table[1]/tbody/tr[1]/td[2]/div/a[6]')[0].click()
+                time.sleep(4)
+        except:
+            break
 
-                dados.append(linha)  # Adiciona a linha aos dados
+    df = pd.DataFrame(rows).drop_duplicates()
+    df.to_csv(os.path.join(final_path, f'banco_decisoes_paper3_comparison_{file_number}.csv'),index=False)
+    rows = list()
+    file_number += 1
 
-    # Retorna os dados como um DataFrame
-    # pd.DataFrame(dados).to_excel(f"decisoes_tjsp_{date.today()}.xlsx", index=False)
-    return pd.DataFrame(dados)
-
-
-
-# Lista de links
-# Execução
-
-import pandas as pd
-
-input_data = pd.read_excel('dados_sentencas.xlsx')
-input_data.input2 = input_data.PROCESSO.str[-4:]
-links = list()
-df = pd.DataFrame()
-for _,row in input_data.iterrows():
-    link = f'https://esaj.tjsp.jus.br/cjpg/pesquisar.do;jsessionid=A3B093BF00038BA60208A3DFE512B566.cjpg1?conversationId=&dadosConsulta.pesquisaLivre=&tipoNumero=UNIFICADO&numeroDigitoAnoUnificado={row.input1}&foroNumeroUnificado={row.input2}&dadosConsulta.nuProcesso={row.PROCESSO}&dadosConsulta.nuProcessoAntigo=&classeTreeSelection.values=&classeTreeSelection.text=&assuntoTreeSelection.values=&assuntoTreeSelection.text=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&cdAgente=&nmAgente=&dadosConsulta.dtInicio=&dadosConsulta.dtFim=&varasTreeSelection.values=&varasTreeSelection.text=&dadosConsulta.ordenacao=DESC'
-    links.append(link)
-df = extract_decisoes_tjsp_grau1(links)
-df.to_excel(f"decisoes_tjsp_{date.today()}.xlsx", index=False)
+driver.quit()
